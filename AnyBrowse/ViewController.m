4bb6fe1d464e36cd30240fe114e2716a217fe0b6
VladanR <x3m.rocker.035@gmail.com>
2020-05-26 15:15:36 +0200

    Add functionality and wrap up the app

--- AnyBrowse/ViewController.m
+++ AnyBrowse/ViewController.m
@@ -16,19 +16,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+   
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.google.com"]]];
+   
+    [self.webView addSubview:self.actIndicator];
+    
+    self.webView.navigationDelegate = self;
 }
 
 
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+    
+    [self.actIndicator startAnimating];
+
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    
+    [self.actIndicator stopAnimating];
+    
+}
+
 - (IBAction)forward:(id)sender {
+    
+    if ([self.webView canGoForward]) {
+           
+         [self.webView goForward];
+           
+       }
+    
 }
 
 - (IBAction)back:(id)sender {
+    
+    if ([self.webView canGoBack]) {
+        
+      [self.webView goBack];
+        
+    }
+    
 }
 
 - (IBAction)refresh:(id)sender {
+    
+    [self.webView reload];
+    
 }
 
 - (IBAction)stop:(id)sender {
+    
+    [self.webView stopLoading];
+    
 }
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://%@", self.searchBar.text]]]];
+    [searchBar resignFirstResponder];
+    
+}
+
 @end
